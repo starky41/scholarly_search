850f0d5f6f48be7a51a117df0a2a726c6ab191a5
starky <balakinmax41@gmail.com>
2023-05-09 23:31:18 +1200

    added cluster names to json

--- dags/tf_idf.py
+++ dags/tf_idf.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import json
@@ -7,6 +9,7 @@ json_path = "output/metadata/arxiv.json"
 json_output = "output/metadata/arxiv_tf.json"
 with open(json_path) as f:
     metadata = json.load(f)
+
 
 # Extract the text data
 text_data = []
@@ -35,20 +38,23 @@ feature_phrases = vectorizer.get_feature_names()
 kmeans_model = KMeans(n_clusters=5)
 kmeans_model.fit(tfidf.toarray())
 
-# Print the top 10 keywords for each cluster
+# Define cluster names based on keywords
+cluster_names = {}
 for i in range(5):
-    print(f"\nTop 10 keywords in cluster {i}:")
     cluster = kmeans_model.cluster_centers_[i]
     top_keywords = [feature_names[idx] for idx in cluster.argsort()[-10:]]
-    print(top_keywords)
+    name = " ".join(top_keywords)
+    cluster_names[i] = name
 
-# Add the keywords and phrases to the metadata JSON file
-
+# Add cluster names to metadata
 for i, doc in enumerate(metadata):
     doc['tf_idf'] = {}
     doc['tf_idf']['keywords'] = [feature_names[idx] for idx in tfidf[i].indices]
     doc['tf_idf']['key_phrases'] = [feature_phrases[idx] for idx in tfidf_phrases[i].indices]
+    doc['tf_idf']['cluster_name'] = cluster_names[kmeans_model.labels_[i]]
+    doc['tf_idf']['cluster_number'] = kmeans_model.labels_[i]
 
+metadata = json.loads(json.dumps(metadata, default=lambda o: int(o) if isinstance(o, np.int32) else o))
 # Save the updated metadata JSON file
 with open(json_output, 'w') as f:
     json.dump(metadata, f)
